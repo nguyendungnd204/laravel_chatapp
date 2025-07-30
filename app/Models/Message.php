52827584239faaadc54cd82cc62c93faa9cb5b83
nguyendungnd204 <ngvandungnvd204@gmail.com>
2025-07-30 09:09:00 +0700

    add relationship in model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'messgae',
+        'sender_id',
+        'group_id',
+        'receiver_id'
+    ];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
 }
